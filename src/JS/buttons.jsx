ff1e5fed186a8df9507f4f8cfe48672deee518b4
Amsssv <Amsssv@yandex.ru>
2022-04-05 19:19:09 +0300

    made the task

--- src/JS/buttons.jsx
+++ src/JS/buttons.jsx
@@ -2,44 +2,48 @@ import React, { useState } from "react";
 
 const Buttons = () => {
 
-	// const buttons = [
-	// 	{
-	// 		name: "First",
-	// 		id: "first",
-	// 	},
-	// 	{
-	// 		name: "Second",
-	// 		id: "second",
-	// 	},
-	// 	{
-	// 		name: "Third",
-	// 		id: "third",
-	// 	},
-	// ]
+	const buttons = [
+		{
+			name: "First",
+			id: "first",
+		},
+		{
+			name: "Second",
+			id: "second",
+		},
+		{
+			name: "Third",
+			id: "third",
+		},
+	]
 
-		const [count, setCount] = useState(0)
+	const setClass = (e) => {
+		let images = document.querySelectorAll(".card__image");
+		let buttons = document.querySelectorAll('.radio__label');
 
-		const addCount = () => {
-			console.log(count + 1)
+		const indexOf = element => Array.from(buttons).indexOf(element);
+
+		const buttonIndex = indexOf(e.currentTarget);
+
+		for (let i = 0; i < buttons.length; i++) {
+			images[i].classList.remove('card__image--active');
 		}
+		images[buttonIndex].classList.add('card__image--active');
 
+
+	}
 
 	return (
 		<div className="body" >
-				<label className="radio" key="first"  >
-					<input type="radio" id="first" className="radio__input" name="selector" />
-					<span className="radio__label" onClick={addCount}>First</span>
+			{buttons.map((button) =>
+				<label className="radio" key={button.id}  >
+					<input type="radio" id={button.id} className="radio__input" name="selector" />
+					<span className="radio__label" onClick={setClass}>{button.name}</span>
 				</label>
-				<label className="radio" key="second" >
-					<input type="radio" id="second" className="radio__input" name="selector" />
-					<span className="radio__label">Second</span>
-				</label>
-				<label className="radio" key="third" >
-					<input type="radio" id="third" className="radio__input" name="selector" />
-					<span className="radio__label">Third</span>
-				</label>
+			)}
 		</div>
 	)
 }
 
 export default Buttons;
+
--- src/JS/images.jsx
+++ src/JS/images.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Images = () => {
 
-    return(
+    return (
         <div className="cards">
             <div className="card__block">
-                <img src="images/ocean.jpg" alt="ocean" className="card__image"></img>
+                <img src="images/ocean.jpg" alt="ocean" className="card__image" ></img>
             </div>
             <div className="card__block">
-                <img src="images/mountain.jpg" alt="mountain" className="card__image card__image--active"></img>
+                <img src="images/mountain.jpg" alt="mountain" className="card__image" ></img>
             </div>
             <div className="card__block">
-                <img src="images/desert.jpg" alt="desert" className="card__image"></img>
+                <img src="images/desert.jpg" alt="desert" className="card__image" ></img>
             </div>
         </div>
     )
